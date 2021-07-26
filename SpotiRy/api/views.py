7219e10a1b5de8627ba1c85c6c6ad90017473c7b
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-07-26 05:49:34 +0300

    Add GetRoomView @ BE

--- SpotiRy/api/views.py
+++ SpotiRy/api/views.py
@@ -40,3 +40,21 @@ class CreateRoomView(APIView):
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GetRoomView(APIView):
+    serializer_class = RoomSerializer
+    lookup_url_kwarg = 'code'
+
+    def get(self, request, format=None):
+        code = request.GET.get(self.lookup_url_kwarg)
+
+        if code != None:
+            room = Room.objects.filter(code=code)
+            if len(room) > 0:
+                data = RoomSerializer(room[0]).data
+                data['is_host'] = self.request.session.session_key == room[0].host
+                return Response(data, status=status.HTTP_200_OK)
+            return Response({'There is no room with such code... :('}, status=status.HTTP_404_NOT_FOUND)
+
+        return Response({"I have not found a code parameter in request"}, status=status.HTTP_400_BAD_REQUEST)
